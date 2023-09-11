74ecdc6b5954e57e7a3e31374b456a36fc19b355
AVan-Git <hoangkc35@gmail.com>
2023-09-11 16:35:23 +0700

    Err: GrantAccess - la bang nhánh ket noi 2 bang n-n -- chua biet sua

--- week1_demoServlet/src/main/java/vn/edu/iuh/fit/week1_demoservlet/repositories/GrantAccessRepositories.java
+++ week1_demoServlet/src/main/java/vn/edu/iuh/fit/week1_demoservlet/repositories/GrantAccessRepositories.java
@@ -1,4 +1,48 @@
 package vn.edu.iuh.fit.week1_demoservlet.repositories;
 
+import vn.edu.iuh.fit.week1_demoservlet.connection.ConnectionDB;
+import vn.edu.iuh.fit.week1_demoservlet.models.GrantAccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class GrantAccessRepositories {
+    private Connection connect = null;
+    public GrantAccessRepositories() {
+        try {
+            connect =  new ConnectionDB().getConnectionBD();
+        }
+        catch (Exception e)
+        {
+            System.err.println("GrantAccessRepositories");
+            System.err.println(e);
+        }
+    }
+    //GrantAccess(
+    //      String account_id
+    //      , String role_id
+    //      , String is_grant
+    //      , String note )
+
+    //insert
+    public void insert(GrantAccess val) throws Exception{
+        String sql = "INSERT INTO GrantAcces VALUES (?,?,?,?)";
+        PreparedStatement ps = connect.prepareStatement(sql);
+        ps.setString(1, val.getAccount_id());
+        ps.setString(2, val.getRole_id());
+        ps.setString(3, val.getIs_grant());
+        ps.setString(4, val.getNote());
+        ps.executeUpdate();
+    }
+
+    //update
+    public void update(GrantAccess val) throws Exception{
+        System.err.println("err update GrantAccess -- chua biet where lay cai nao");
+//        String sql = "UPDATE GrantAccess set account_id=?, role_id=?, is_grant=?, note=? WHERE account_id=? ";
+//        PreparedStatement ps = connect.prepareStatement(sql);
+    }
+    //del
+
+    //getbyid
+    //getall
 }
