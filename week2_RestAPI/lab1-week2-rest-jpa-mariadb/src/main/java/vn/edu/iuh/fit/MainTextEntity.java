4eb842acaf3d30db33d19ad7c0aaa2658e241802
AVan-Git <hoangkc35@gmail.com>
2023-09-28 18:14:37 +0700

    finish entity - product - data local

--- week2_RestAPI/lab1-week2-rest-jpa-mariadb/src/main/java/vn/edu/iuh/fit/MainTextEntity.java
+++ week2_RestAPI/lab1-week2-rest-jpa-mariadb/src/main/java/vn/edu/iuh/fit/MainTextEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import vn.edu.iuh.fit.enums.EmployeeStatus;
+import vn.edu.iuh.fit.enums.ProductStatus;
 import vn.edu.iuh.fit.models.Employee;
+import vn.edu.iuh.fit.models.Product;
 
 public class MainTextEntity {
 
@@ -16,12 +18,19 @@ public class MainTextEntity {
         emp.setAddress("12 phan co ich");
         emp.setStatus(EmployeeStatus.IN_ACTIVE);
 
+        Product pro1 = new Product();
+        pro1.setName("Banh gao 1");
+        pro1.setUnit("don vi 1");
+        pro1.setDescription("Mo ta 1");
+        pro1.setManufacturerName("NSX Banh VN");
+        pro1.setStatus(ProductStatus.IN_ACTIVE);
+
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         entityManager.getTransaction().begin();
-        entityManager.persist(emp);
+        entityManager.persist(pro1);
         entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
