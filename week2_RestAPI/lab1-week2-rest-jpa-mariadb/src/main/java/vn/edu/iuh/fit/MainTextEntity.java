02d1554f674f3c3ee627e6e7a0b90f064acc50be
AVan-Git <hoangkc35@gmail.com>
2023-09-29 08:57:48 +0700

    add entity - customer - data

--- week2_RestAPI/lab1-week2-rest-jpa-mariadb/src/main/java/vn/edu/iuh/fit/MainTextEntity.java
+++ week2_RestAPI/lab1-week2-rest-jpa-mariadb/src/main/java/vn/edu/iuh/fit/MainTextEntity.java
@@ -5,16 +5,23 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import vn.edu.iuh.fit.enums.EmployeeStatus;
 import vn.edu.iuh.fit.enums.ProductStatus;
+import vn.edu.iuh.fit.models.Customer;
 import vn.edu.iuh.fit.models.Employee;
 import vn.edu.iuh.fit.models.Product;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class MainTextEntity {
 
     public static void main(String[] args) {
+
+        LocalDate date = LocalDate.of(2020, 1, 8);
         Employee emp = new Employee();
         emp.setName("Nguyen Van 1");
         emp.setEmail("email@gmail.com");
         emp.setPhone("0951753001");
+        emp.setDob(date);
         emp.setAddress("12 phan co ich");
         emp.setStatus(EmployeeStatus.IN_ACTIVE);
 
@@ -25,12 +32,18 @@ public class MainTextEntity {
         pro1.setManufacturerName("NSX Banh VN");
         pro1.setStatus(ProductStatus.IN_ACTIVE);
 
+        Customer cus = new Customer();
+        cus.setName("Tran binh");
+        cus.setEmail("Binh@gmail.com");
+        cus.setPhone("0951753002");
+        cus.setAddress("12 phan co ich");
+
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         entityManager.getTransaction().begin();
-        entityManager.persist(pro1);
+        entityManager.persist(cus);
         entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
---
+++ week2_RestAPI/lab1-week2-rest-jpa-mariadb/src/main/java/vn/edu/iuh/fit/models/Customer.java
@@ -0,0 +1,89 @@
+package vn.edu.iuh.fit.models;
+
+import jakarta.persistence.*;
+
+/**
+ * customer (cust_id, cust_name, email, phone, address)
+ *
+ * -- Long - String - String - String - String -
+ *
+ * -- Customer(long id, String name, String email, String phone, String address)
+ */
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue
+    @Column(name = "cust_id")
+    private long id;
+    @Column(name = "cust_name", nullable = false)
+    private String name;
+    @Column(name = "email", unique = true, length = 50)
+    private String email;
+    @Column(name = "phone", unique = true, length = 15)
+    private String phone;
+    @Column(name = "address")
+    private String address;
+
+    public Customer() {
+    }
+
+    public Customer(long id, String name, String email, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
