49a7737b0b78705517936b8d74979141d75bb914
unknown <hoangkc35@gmail.com>
2023-10-07 07:23:06 +0700

    start week5 - lab1

---
+++ week5_SpringBoot_JPA_Mariadb/demo-springboot-jpa-mariadb/src/main/java/vn/edu/iuh/fit/se/demospringbootjpamariadb/DemoSpringbootJpaMariadbApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se.demospringbootjpamariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootJpaMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootJpaMariadbApplication.class, args);
+	}
+
+}
---
+++ week5_SpringBoot_JPA_Mariadb/demo-springboot-jpa-mariadb/src/test/java/vn/edu/iuh/fit/se/demospringbootjpamariadb/DemoSpringbootJpaMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se.demospringbootjpamariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootJpaMariadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
