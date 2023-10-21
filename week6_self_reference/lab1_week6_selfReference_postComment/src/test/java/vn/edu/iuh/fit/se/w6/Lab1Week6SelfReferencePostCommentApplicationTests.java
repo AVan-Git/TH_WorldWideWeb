48046460e680472ab2a445a12fe3a858b5149e57
unknown <hoangkc35@example.com>
2023-10-21 07:37:37 +0700

    start week 6

---
+++ week6_self_reference/lab1_week6_selfReference_postComment/src/test/java/vn/edu/iuh/fit/se/w6/Lab1Week6SelfReferencePostCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se.w6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Week6SelfReferencePostCommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
