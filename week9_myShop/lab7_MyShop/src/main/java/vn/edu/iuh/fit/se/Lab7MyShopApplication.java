7e35a27dc31c6f5874a84b9ca216c6306762dbc8
unknown <hoangkc35@example.com>
2023-11-04 07:42:22 +0700

    start w9 - BT Ql cửa hàng

---
+++ week9_myShop/lab7_MyShop/src/main/java/vn/edu/iuh/fit/se/Lab7MyShopApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7MyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7MyShopApplication.class, args);
+    }
+
+}
---
+++ week9_myShop/lab7_MyShop/src/test/java/vn/edu/iuh/fit/se/Lab7MyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7MyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
